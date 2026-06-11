84ff943bf4935934fdc202d89048bcff59ed1854
qme501@usask.ca <adeboyeore091@gmail.com>
2026-06-10 18:45:22 -0600

    added a scroll to top function in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Waitlist from './pages/Waitlist';
 import NotFound from './pages/NotFound';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
